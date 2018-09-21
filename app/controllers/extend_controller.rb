621999e31712e949d872e9aa47de62182017513e
Kelly J Andrews <kelly@kellyjandrews.com>
2018-09-21 13:28:15 -0400

    style updates, nav changes

--- app/controllers/extend_controller.rb
+++ app/controllers/extend_controller.rb
@@ -1,24 +1,42 @@
 class ExtendController < ApplicationController
+  before_action :set_header
+
   def index  
     document_paths = Dir.glob("#{Rails.root}/_extend/**/*.md")
 
     @extensions = document_paths.map do |document_path|
       document = File.read(document_path)
-      body = MarkdownPipeline.new.call(document)
       frontmatter = YAML.safe_load(document)
       title = frontmatter['title']
       description = frontmatter['description']
       tags = frontmatter['tags'] || []
       languages = frontmatter['languages'] || []
       image = frontmatter['image'] || ''
-      { title: title, description: description, tags: tags, languages: languages, image: image }
+      route = File.basename(document_path, ".*")
+      { title: title, description: description, tags: tags, languages: languages, image: image, route: route }
     end
 
       render layout: 'page'
     end
   
     def show
+      document_path = "#{Rails.root}/_extend/#{params[:title]}.md"
+
+      document = File.read(document_path)
+      body = MarkdownPipeline.new.call(document)
+      frontmatter = YAML.safe_load(document)
+      title = frontmatter['title']
+
+      @extension = { title: title }
+
       render layout: 'page'
     end
+
+    private
+
+      def set_header
+        @navigation = :extend
+      end
   end
+
   
