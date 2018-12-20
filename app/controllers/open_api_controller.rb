4309e8e0e812a4db7f39ca79d44854c0ecf9d980
Michael Heap <m@michaelheap.com>
2018-12-20 09:41:18 +0000

    Support OAS documents that do not contain tags

--- app/controllers/open_api_controller.rb
+++ app/controllers/open_api_controller.rb
@@ -39,19 +39,24 @@ class OpenApiController < ApplicationController
   end
 
   def set_groups
+    tags = @definition.raw['tags']
     # For now we only use the first tag in the list as an equivalent for the old x-group functionality
-    @groups = @definition.endpoints.group_by { |endpoint| endpoint.raw['tags'].first }
+    @groups = @definition.endpoints.group_by do |endpoint|
+      next nil unless tags
+      endpoint.raw['tags'].first
+    end
 
     # We want to use the order in which the tags are defined in the definition, so iterate over the tags
     # and store the index against the tag name. We'll use this later for sorting
     ordering = {}
-    @definition.raw['tags'].each_with_index do |tag, index|
+    tags&.each_with_index do |tag, index|
       ordering[tag['name'].capitalize] = index
     end
 
     # Sort by the order in which they're defined in the definition
     @groups = @groups.sort_by do |name, _|
-      ordering[name.capitalize]
+      return 999 if name.nil?
+      ordering[name.capitalize] || 999
     end
   end
 
