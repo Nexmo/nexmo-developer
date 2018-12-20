cc82d07906e411fb16a1fd0ab30cd039405e27a7
benhayehudi <bengreenberg@gmail.com>
2018-12-20 15:13:54 +0200

    added another test for #normalize_summary_title

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -5,5 +5,8 @@ RSpec.describe ApplicationHelper, :type => :helper do
         it 'should return summary if it is not null' do
             expect(helper.normalize_summary_title("Summary Here", "operationID")).to eql("Summary Here")
         end
+        it 'should return operationID if summary is null' do
+            expect(helper.normalize_summary_title(nil, "operationId")). to eql("Operation Id")
+        end
     end
 end
