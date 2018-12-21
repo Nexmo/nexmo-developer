c2f35e857acd6e77c892dbb0fb1e4c5a88bc0b25
benhayehudi <bengreenberg@gmail.com>
2018-12-21 15:18:17 +0200

    rubocop fixes

--- spec/filters/break_filter_spec.rb
+++ spec/filters/break_filter_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe BreakFilter do
   it 'turns § symbol into a break HTML tag' do
     input = <<~HEREDOC
-        §
+      §
     HEREDOC
 
     expected_output = <<~HEREDOC
@@ -14,22 +14,22 @@ RSpec.describe BreakFilter do
   end
   it 'turns more than one instance of § into an equal number of break HTML tags' do
     input = <<~HEREDOC
-        §§
+      §§
     HEREDOC
 
     expected_output = <<~HEREDOC
-        <br><br>
+      <br><br>
     HEREDOC
 
     expect(BreakFilter.call(input)). to eq(expected_output)
   end
-  it 'only transforms the § symbol and does not turn anything else into a break HTML tag' do 
+  it 'only transforms the § symbol and does not turn anything else into a break HTML tag' do
     input = <<~HEREDOC
-        not a symbol
+      not a symbol
     HEREDOC
 
     expected_output = <<~HEREDOC
-        not a symbol
+      not a symbol
     HEREDOC
 
     expect(BreakFilter.call(input)). to eq(expected_output)
