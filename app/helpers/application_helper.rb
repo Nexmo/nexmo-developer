78b9eeb0a43999b509f03f86074be31863ba35df
Michael Heap <m@michaelheap.com>
2018-12-22 09:11:05 +0000

    Refactor summary title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -209,28 +209,23 @@ module ApplicationHelper
   end
 
   def normalize_summary_title(summary, operation_id)
-    # define the regex for the matches
-    camel_match = /^[a-zA-Z]\w+(?:[A-Z]\w+){1,}/x
-
-    # define list of terms that must be made all uppercase
-    uppercase_array = ['SMS']
-
-    # return summary and exit if it is provided
+    # return summary early if provided
     return summary unless summary.nil?
 
-    if operation_id.match?(camel_match)
-      summary = operation_id.underscore
-    else
-      summary = operation_id
+    # If the operation ID is camelCase,
+    if operation_id.match?(/^[a-zA-Z]\w+(?:[A-Z]\w+){1,}/x)
+      # Use the rails `.underscore` method to convert someString to some_string
+      operation_id = operation_id.underscore
     end
 
-    summary = summary.gsub(/(_|-)/, ' ').titleize
+    # Replace snake_case and kebab-case with spaces and titelize the string
+    operation_id = operation_id.gsub(/(_|-)/, ' ').titleize
 
-    summary = summary.split(' ').map do |c|
+    # Some terms need to be capitalised all the time
+    uppercase_array = ['SMS', 'DTMF']
+    operation_id.split(' ').map do |c|
       next c.upcase if uppercase_array.include?(c.upcase)
       c
     end.join(' ')
-
-    summary
   end
 end
