4163f8371f03e8cdb2bc851f478e3170eb29d997
benhayehudi <bengreenberg@gmail.com>
2018-12-24 12:07:06 +0200

    rubocop

--- spec/filters/indent_filter_spec.rb
+++ spec/filters/indent_filter_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe IndentFilter do 
-    it 'turns four whitespaces followed by "->" into indented text' do
-        input = "    -> a"
+RSpec.describe IndentFilter do
+  it 'turns four whitespaces followed by "->" into indented text' do
+    input = '    -> a'
 
-        expected_output = "<div class=\"indent\"> <p>a</p></div>"
+    expected_output = '<div class="indent"> <p>a</p></div>'
 
-        expect(described_class.call(input)).to eq(expected_output)
-    end
+    expect(described_class.call(input)).to eq(expected_output)
+  end
 end
