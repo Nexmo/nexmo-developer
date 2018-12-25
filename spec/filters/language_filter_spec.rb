3a409051281e4260b67bc722f76e028f581804c0
benhayehudi <bengreenberg@gmail.com>
2018-12-25 14:14:45 +0200

    rubocop fixes

--- spec/filters/language_filter_spec.rb
+++ spec/filters/language_filter_spec.rb
@@ -1,35 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe LanguageFilter do
-    it 'does nothing to text that does not match the regex' do 
-        input = 'some text'
+  it 'does nothing to text that does not match the regex' do
+    input = 'some text'
 
-        expected_output = 'some text'
-        
-        expect(described_class.call(input)).to eql(expected_output)
-    end
+    expected_output = 'some text'
 
-    it 'raises an error if input is whitespace' do 
-        input = ' '
+    expect(described_class.call(input)).to eql(expected_output)
+  end
 
-        expected_output = ' '
+  it 'raises an error if input is whitespace' do
+    input = ' '
 
-        expect{described_class(input)}.to raise_error(ArgumentError)
-    end
+    expected_output = ' ' # rubocop:disable UselessAssignment
 
-    it 'wraps matching input in a span tag and assigns "lang" to the tag' do 
-        input = "[נצמו](lang: 'il')"
+    expect { described_class(input) }.to raise_error(ArgumentError)
+  end
 
-        expected_output = "<span lang='il'>נצמו</span>"
+  it 'wraps matching input in a span tag and assigns "lang" to the tag' do
+    input = "[נצמו](lang: 'il')"
 
-        expect(described_class.call(input)).to eql(expected_output)
-    end
+    expected_output = "<span lang='il'>נצמו</span>"
 
-    it 'does nothing if the text is in correct format but it is missing the correct formatting' do 
-        input = "Bonjour(lang: 'fr')"
+    expect(described_class.call(input)).to eql(expected_output)
+  end
 
-        expected_output = "Bonjour(lang: 'fr')"
+  it 'does nothing if the text is in correct format but it is missing the correct formatting' do
+    input = "Bonjour(lang: 'fr')"
 
-        expect(described_class.call(input)).to eql(expected_output)
-    end
+    expected_output = "Bonjour(lang: 'fr')"
+
+    expect(described_class.call(input)).to eql(expected_output)
+  end
 end
