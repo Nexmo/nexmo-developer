6e62e1a44816b3093db40d95303fc3abc0d06bfe
benhayehudi <bengreenberg@gmail.com>
2018-12-25 11:59:30 +0200

    rubocop fixes

--- spec/filters/label_filter_spec.rb
+++ spec/filters/label_filter_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe LabelFilter do 
+RSpec.describe LabelFilter do
   it 'does not transform a random string' do
-    input = "some text"
+    input = 'some text'
 
-    expected_output = "some text"
-    
+    expected_output = 'some text'
+
     expect(described_class.call(input)).to eq(expected_output)
   end
 
   it 'returns an HTML span tag when provided with random text inside brackets' do
-    input = "[some text]"
+    input = '[some text]'
 
     expected_output = "<span class='Vlt-badge '>some text</span> "
 
@@ -18,15 +18,15 @@ RSpec.describe LabelFilter do
   end
 
   it 'does not transform a matching string if it is not inside brackets' do
-    input = "POST"
+    input = 'POST'
 
-    expected_output = "POST"
+    expected_output = 'POST'
 
     expect(described_class.call(input)).to eq(expected_output)
   end
 
   it 'returns a green HTML class when string "[POST]" is provided and puts "POST" in between <span> tags stripped of brackets' do
-    input = "[POST]"
+    input = '[POST]'
 
     expected_output = "<span class='Vlt-badge Vlt-badge--green'>POST</span> "
 
@@ -34,7 +34,7 @@ RSpec.describe LabelFilter do
   end
 
   it 'returns a blue HTML class when string "[GET]" is provided and puts "GET" in between <span> tags stripped of brackets' do
-    input = "[GET]"
+    input = '[GET]'
 
     expected_output = "<span class='Vlt-badge Vlt-badge--blue'>GET</span> "
 
@@ -42,7 +42,7 @@ RSpec.describe LabelFilter do
   end
 
   it 'returns a red HTML class when string "[DELETE]" is provided and puts "DELETE" in between <span> tags stripped of brackets' do
-    input = "[DELETE]"
+    input = '[DELETE]'
 
     expected_output = "<span class='Vlt-badge Vlt-badge--red'>DELETE</span> "
 
@@ -50,16 +50,16 @@ RSpec.describe LabelFilter do
   end
 
   it 'returns a yellow HTML class when string "[PUT]" is provided and puts "PUT" in between <span> tags stripped of brackets' do
-    input = "[PUT]"
+    input = '[PUT]'
 
     expected_output = "<span class='Vlt-badge Vlt-badge--yellow'>PUT</span> "
 
     expect(described_class.call(input)).to eq(expected_output)
   end
-  
+
   # possible undesired behavior to be discussed
-  it 'returns a non-color HTML span tag with  "post" in between the tags when "[post]" is provided' do 
-    input = "[post]"
+  it 'returns a non-color HTML span tag with  "post" in between the tags when "[post]" is provided' do
+    input = '[post]'
 
     expected_output = "<span class='Vlt-badge '>post</span> "
 
