05199337bec261af2d7b98ccd5f88153464b0faf
Michael Heap <m@michaelheap.com>
2019-01-02 09:40:54 +0000

    Add missing space to IndentFilter input

--- spec/filters/indent_filter_spec.rb
+++ spec/filters/indent_filter_spec.rb
@@ -2,9 +2,14 @@ require 'rails_helper'
 
 RSpec.describe IndentFilter do
   it 'turns four whitespaces followed by "->" into indented text' do
-    input = '   -> a'
+    input = '    -> a'
 
-    expected_output = '<div class="indent"> <p>a</p></div>'
+    expected_output = <<~HEREDOC
+      <div class="indent">
+        <p>a</p>
+
+      </div>
+    HEREDOC
 
     expect(described_class.call(input)).to eq(expected_output)
   end
