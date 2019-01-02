20bef914b10328387fa32da1f6709c046baa8522
benhayehudi <bengreenberg@gmail.com>
2019-01-02 15:36:52 +0200

    added two additional tests to check for behavior if path is missing and if title is missing

--- spec/filters/techio_filter_spec.rb
+++ spec/filters/techio_filter_spec.rb
@@ -44,4 +44,42 @@ RSpec.describe TechioFilter do
 
     expect { described_class.call(input) }.to raise_error(NoMethodError)
   end
+
+  it 'returns HTML and JS script tags with no title in the iframe src path if title is missing' do
+    input = <<~HEREDOC
+      ```techio
+      path: /here/is/a/path
+      title:
+      ```
+    HEREDOC
+
+    expected_output = <<~HEREDOC
+      <div class="techio-container">
+        <iframe width="100%" frameborder="0" scrolling="no" allowtransparency="true" style="visibility: hidden" src="https://tech.io/playground-widget/here/is/a/path/"></iframe>
+        <script>if(typeof window.techioScriptInjected==="undefined"){window.techioScriptInjected=true;var d=document,s=d.createElement("script");s.src="https://files.codingame.com/codingame/iframe-v-1-4.js";(d.head||d.body).appendChild(s);}</script>
+      </div>
+    HEREDOC
+
+    # .chop off trailing \n from input
+    expect(described_class.call(input.chop)).to eql(expected_output)
+  end
+
+  it 'returns HTML and JS script tags with no path in the iframe src if path is missing' do
+    input = <<~HEREDOC
+      ```techio
+      path:
+      title: this is a title
+      ```
+    HEREDOC
+
+    expected_output = <<~HEREDOC
+      <div class="techio-container">
+        <iframe width="100%" frameborder="0" scrolling="no" allowtransparency="true" style="visibility: hidden" src="https://tech.io/playground-widget/this is a title"></iframe>
+        <script>if(typeof window.techioScriptInjected==="undefined"){window.techioScriptInjected=true;var d=document,s=d.createElement("script");s.src="https://files.codingame.com/codingame/iframe-v-1-4.js";(d.head||d.body).appendChild(s);}</script>
+      </div>
+    HEREDOC
+
+    # .chop off trailing \n from input
+    expect(described_class.call(input.chop)).to eql(expected_output)
+  end
 end
