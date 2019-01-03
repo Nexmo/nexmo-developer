942ba011f41f132467edc1a4c6cdd5cdd822e2f4
benhayehudi <bengreenberg@gmail.com>
2019-01-03 10:31:03 +0200

    added two new tests to check for behavior if title or path are completely missing

--- spec/filters/techio_filter_spec.rb
+++ spec/filters/techio_filter_spec.rb
@@ -82,4 +82,40 @@ RSpec.describe TechioFilter do
     # .chop off trailing \n from input
     expect(described_class.call(input.chop)).to eql(expected_output)
   end
+
+  it 'returns HTML and JS script tags with no title in the iframe src path if title is completely missing' do
+    input = <<~HEREDOC
+      ```techio
+      path: /here/is/a/path
+      ```
+    HEREDOC
+
+    expected_output = <<~HEREDOC
+      <div class="techio-container">
+        <iframe width="100%" frameborder="0" scrolling="no" allowtransparency="true" style="visibility: hidden" src="https://tech.io/playground-widget/here/is/a/path/"></iframe>
+        <script>if(typeof window.techioScriptInjected==="undefined"){window.techioScriptInjected=true;var d=document,s=d.createElement("script");s.src="https://files.codingame.com/codingame/iframe-v-1-4.js";(d.head||d.body).appendChild(s);}</script>
+      </div>
+    HEREDOC
+
+    # .chop off trailing \n from input
+    expect(described_class.call(input.chop)).to eql(expected_output)
+  end
+
+  it 'returns HTML and JS script tags with no path in the iframe src if path is completely missing' do
+    input = <<~HEREDOC
+      ```techio
+      title: this is a title
+      ```
+    HEREDOC
+
+    expected_output = <<~HEREDOC
+      <div class="techio-container">
+        <iframe width="100%" frameborder="0" scrolling="no" allowtransparency="true" style="visibility: hidden" src="https://tech.io/playground-widget/this is a title"></iframe>
+        <script>if(typeof window.techioScriptInjected==="undefined"){window.techioScriptInjected=true;var d=document,s=d.createElement("script");s.src="https://files.codingame.com/codingame/iframe-v-1-4.js";(d.head||d.body).appendChild(s);}</script>
+      </div>
+    HEREDOC
+
+    # .chop off trailing \n from input
+    expect(described_class.call(input.chop)).to eql(expected_output)
+  end
 end
