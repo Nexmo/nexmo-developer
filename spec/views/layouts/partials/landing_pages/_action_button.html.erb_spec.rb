82c82ce513c53e049e9f6c35b1cd397e62296bff
benhayehudi <bengreenberg@gmail.com>
2019-01-29 11:05:21 +0200

    rubocop fixes

--- spec/views/layouts/partials/landing_pages/_action_button.html.erb_spec.rb
+++ spec/views/layouts/partials/landing_pages/_action_button.html.erb_spec.rb
@@ -1,11 +1,11 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "rendering _action_button landing page partial" do
-  it "renders correctly with local variables" do
+RSpec.describe 'rendering _action_button landing page partial' do
+  it 'renders correctly with local variables' do
     url_input = '#a-sample-url'
     text_input = 'Click here!'
 
-    render :partial => "/layouts/partials/landing_pages/action_button.html.erb", :locals => {'url' => url_input, 'text' => text_input}
+    render partial: '/layouts/partials/landing_pages/action_button.html.erb', locals: { 'url' => url_input, 'text' => text_input }
 
     expect(rendered).to include('<a class="Vlt-btn Vlt-btn--primary" href="#a-sample-url">')
     expect(rendered).to include('Click here!')
