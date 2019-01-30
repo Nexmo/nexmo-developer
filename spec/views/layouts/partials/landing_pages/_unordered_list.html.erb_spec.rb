6f316cb70f0bc93d462f084bbb6d83f0aa5be47a
benhayehudi <bengreenberg@gmail.com>
2019-01-30 09:04:09 +0200

    refactored _unordered_list partial to check that array is present and added more tests on partial

--- spec/views/layouts/partials/landing_pages/_unordered_list.html.erb_spec.rb
+++ spec/views/layouts/partials/landing_pages/_unordered_list.html.erb_spec.rb
@@ -19,4 +19,26 @@ RSpec.describe 'rendering _unordered_list landing page partial' do
     expect(rendered).to include('Item 2')
     expect(rendered).to include('Item 3')
   end
+
+  it 'renders ul without a bullet shape classname if bullet shape is not provided' do
+    list = [
+        { 'item' => 'Item 1' },
+        { 'item' => 'Item 2' },
+        { 'item' => 'Item 3' },
+      ]
+
+    render partial: '/layouts/partials/landing_pages/unordered_list.html.erb', locals: {
+        'list' => list,
+    }
+
+    expect(rendered).to include('<ul class="Vlt-list Vlt-list--">')
+  end
+
+  it 'renders an empty list if no data is provided' do
+    render partial: '/layouts/partials/landing_pages/unordered_list.html.erb'
+
+    expected_output = '<ul class="Vlt-list Vlt-list--"></ul>'
+
+    #expect(rendered).to eql(expected_output) # need to strip \n from output
+  end
 end
