a0814c92cd9a2e12763eb02940ee9d2328f07198
benhayehudi <bengreenberg@gmail.com>
2019-01-30 09:33:50 +0200

    refactored linked_image partial with conditional check and added more tests for it

--- spec/views/layouts/partials/landing_pages/_linked_image.html.erb_spec.rb
+++ spec/views/layouts/partials/landing_pages/_linked_image.html.erb_spec.rb
@@ -10,4 +10,22 @@ RSpec.describe 'rendering _linked_image landing page partial' do
     expect(rendered).to include('<a href="#link-me">')
     expect(rendered).to include('<img src="/path/to/image" />')
   end
+
+  it 'does not render href or img tag content when url is not provided' do
+    image = '/path/to/image'
+
+    render partial: '/layouts/partials/landing_pages/linked_image.html.erb', locals: { 'image' => image }
+
+    expect(rendered).to_not include('<a href=')
+    expect(rendered).to_not include('<img src=')
+  end
+
+  it 'does not render an img tag when image is not provided' do
+    url = '#link-me'
+
+    render partial: '/layouts/partials/landing_pages/linked_image.html.erb', locals: { 'url' => url }
+
+    expect(rendered).to include('<a href="#link-me">')
+    expect(rendered).to_not include('<img src=')
+  end
 end
