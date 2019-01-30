02ca614a8dd4d08e988cc4043dc6074f2d574201
benhayehudi <bengreenberg@gmail.com>
2019-01-30 14:58:11 +0100

    added conditional logic to structured_text partial and additional tests

--- spec/views/layouts/partials/landing_pages/_structured_text.html.erb_spec.rb
+++ spec/views/layouts/partials/landing_pages/_structured_text.html.erb_spec.rb
@@ -23,4 +23,72 @@ RSpec.describe 'rendering _structured_text landing page partial' do
     expect(rendered).to include('<p class="p-large">')
     expect(rendered).to include('Things here')
   end
+
+  it 'does not render an svg tag with a color if no color is provided' do
+    icon = 'nexmo-circle'
+    header = 'My header'
+    text = [
+      { 'type' => 'small', 'content' => 'Things here' },
+      { 'type' => 'large', 'content' => 'Large things here' },
+    ]
+
+    render partial: '/layouts/partials/landing_pages/structured_text.html.erb', locals: {
+        'icon' => icon,
+        'header' => header,
+        'text' => text,
+    }
+
+    expect(rendered).to include('<svg class="Vlt-">')
+    expect(rendered).to_not include('<svg class="Vlt-blue">')
+  end
+
+  it 'does not render an use tag with an icon if no icon is specified' do
+    icon_color = 'blue'
+    header = 'My header'
+    text = [
+      { 'type' => 'small', 'content' => 'Things here' },
+      { 'type' => 'large', 'content' => 'Large things here' },
+    ]
+
+    render partial: '/layouts/partials/landing_pages/structured_text.html.erb', locals: {
+        'icon_color' => icon_color,
+        'header' => header,
+        'text' => text,
+    }
+
+    expect(rendered).to include('<use xlink:href="/symbol/volta-icons.svg#Vlt-">')
+    expect(rendered).to_not include('<use xlink:href="/symbol/volta-icons.svg#Vlt-nexmo-circle">')
+  end
+
+  it 'does not render a header if no header is provided' do
+    icon_color = 'blue'
+    icon = 'nexmo-circle'
+    text = [
+      { 'type' => 'small', 'content' => 'Things here' },
+      { 'type' => 'large', 'content' => 'Large things here' },
+    ]
+
+    render partial: '/layouts/partials/landing_pages/structured_text.html.erb', locals: {
+        'icon_color' => icon_color,
+        'icon' => icon,
+        'text' => text,
+    }
+
+    expect(rendered).to_not include('My header')
+  end
+
+  it 'does not render text content type if no text is provided' do
+    icon_color = 'blue'
+    icon = 'nexmo-circle'
+    header = 'My header'
+
+    render partial: '/layouts/partials/landing_pages/structured_text.html.erb', locals: {
+        'icon_color' => icon_color,
+        'icon' => icon,
+        'header' => header,
+    }
+
+    expect(rendered).to_not include('<p class="p-large">')
+    expect(rendered).to_not include('<p>')
+  end
 end
