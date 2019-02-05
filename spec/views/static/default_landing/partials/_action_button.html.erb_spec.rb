2aeb0ce02623eae564d329ad78023f2152c42796
benhayehudi <bengreenberg@gmail.com>
2019-02-05 17:01:04 +0200

    refactored action_button conditional rendering

--- spec/views/static/default_landing/partials/_action_button.html.erb_spec.rb
+++ spec/views/static/default_landing/partials/_action_button.html.erb_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe 'rendering _action_button landing page partial' do
   it 'renders correctly with local variables' do
     url_input = '#a-sample-url'
     text_input = 'Click here!',
-    large = false
+                 large = false
     type = 'primary'
 
     render partial: '/static/default_landing/partials/action_button.html.erb', locals: { 'url' => url_input, 'text' => text_input, 'large' => large, 'type' => type }
 
-    expect(rendered).to include('<a class="Vlt-btn Vlt-btn--primary" href="#a-sample-url">')
+    expect(rendered).to include('<a class="Vlt-btn Vlt-btn--primary " href="#a-sample-url">')
     expect(rendered).to include('Click here!')
   end
 
@@ -24,7 +24,7 @@ RSpec.describe 'rendering _action_button landing page partial' do
       'url' => url_input, 'text' => text_input, 'another_variable' => another_variable, 'large' => large, 'type' => type
     }
 
-    expect(rendered).to include('<a class="Vlt-btn Vlt-btn--primary" href="#a-sample-url">')
+    expect(rendered).to include('<a class="Vlt-btn Vlt-btn--primary " href="#a-sample-url">')
     expect(rendered).to include('Click here!')
     expect(rendered).to_not include('Ignore me')
   end
@@ -43,21 +43,5 @@ RSpec.describe 'rendering _action_button landing page partial' do
     type = 'primary'
 
     expect { render partial: '/static/default_landing/partials/action_button.html.erb', locals: { 'url' => url, 'large' => large, 'type' => type } }.to raise_error("missing 'text' key in action_button landing page block")
-  end
-
-  it 'raises an error if large key value is not true or false' do
-    text_input = 'Click here!'
-    url = '#a-sample-url'
-    large = 'hello'
-    type = 'primary'
-
-    expect do
-      render partial: '/static/default_landing/partials/action_button.html.erb', locals: {
-        'url' => url,
-        'text' => text_input,
-        'large' => large,
-        'type' => type,
-      }
-    end.to raise_error("Unknown value for 'large' key: #{large}")
   end
 end
