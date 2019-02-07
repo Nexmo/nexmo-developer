47c8a0249358af3b2ee2439fd38f27e81eda9f17
benhayehudi <bengreenberg@gmail.com>
2019-02-07 09:53:34 +0200

    passing variable definitions directly into structured_text partial tests

--- spec/views/static/default_landing/partials/_structured_text.html.erb_spec.rb
+++ spec/views/static/default_landing/partials/_structured_text.html.erb_spec.rb
@@ -2,21 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'rendering _structured_text landing page partial' do
   it 'renders correctly with local variable' do
-    icon_color = 'blue'
-    icon = 'nexmo-circle'
-    header = 'My header'
-    center_text = false
-    text = [
-      { 'type' => 'small', 'content' => 'Things here' },
-      { 'type' => 'large', 'content' => 'Large things here' },
-    ]
-
     render partial: '/static/default_landing/partials/structured_text.html.erb', locals: {
-        'icon_color' => icon_color,
-        'icon' => icon,
-        'header' => header,
-        'center_text' => center_text,
-        'text' => text,
+        'icon_color' => 'blue',
+        'icon' => 'nexmo-circle',
+        'header' => 'My header',
+        'center_text' => false,
+        'text' => [
+          { 'type' => 'small', 'content' => 'Things here' },
+          { 'type' => 'large', 'content' => 'Large things here' },
+        ],
     }
 
     expect(rendered).to include('<svg class="Vlt-blue">')
@@ -28,70 +22,54 @@ RSpec.describe 'rendering _structured_text landing page partial' do
   end
 
   it 'raises an error if icon color is not provided and center_text is false' do
-    icon = 'nexmo-circle'
-    header = 'My header'
-    center_text = false
-    text = [
-      { 'type' => 'small', 'content' => 'Things here' },
-      { 'type' => 'large', 'content' => 'Large things here' },
-    ]
-
     expect do
       render partial: '/static/default_landing/partials/structured_text.html.erb', locals: {
-        'icon' => icon,
-        'header' => header,
-        'center_text' => center_text,
-        'text' => text,
+        'icon' => 'nexmo-circle',
+        'header' => 'My header',
+        'center_text' => false,
+        'text' => [
+          { 'type' => 'small', 'content' => 'Things here' },
+          { 'type' => 'large', 'content' => 'Large things here' },
+        ],
     }
     end .to raise_error("Missing 'icon_color' key in structured_text landing page block")
   end
 
   it 'raises an error if an icon is not provided and center_text is false' do
-    icon_color = 'blue'
-    header = 'My header'
-    center_text = false
-    text = [
-      { 'type' => 'small', 'content' => 'Things here' },
-      { 'type' => 'large', 'content' => 'Large things here' },
-    ]
-
     expect do
       render partial: '/static/default_landing/partials/structured_text.html.erb', locals: {
-        'icon_color' => icon_color,
-        'header' => header,
-        'text' => text,
-        'center_text' => center_text,
+        'icon_color' => 'blue',
+        'header' => 'My header',
+        'center_text' => false,
+        'text' => [
+          { 'type' => 'small', 'content' => 'Things here' },
+          { 'type' => 'large', 'content' => 'Large things here' },
+        ],
     }
     end .to raise_error("Missing 'icon' key in structured_text landing page block")
   end
 
   it 'raises an error if a header is not provided' do
-    icon_color = 'blue'
-    icon = 'nexmo-circle'
-    text = [
-      { 'type' => 'small', 'content' => 'Things here' },
-      { 'type' => 'large', 'content' => 'Large things here' },
-    ]
-
     expect do
       render partial: '/static/default_landing/partials/structured_text.html.erb', locals: {
-        'icon_color' => icon_color,
-        'icon' => icon,
-        'text' => text,
+        'icon_color' => 'blue',
+        'icon' => 'nexmo-circle',
+        'center_text' => false,
+        'text' => [
+          { 'type' => 'small', 'content' => 'Things here' },
+          { 'type' => 'large', 'content' => 'Large things here' },
+        ],
     }
     end .to raise_error("Missing 'header' key in structured_text landing page block")
   end
 
   it 'raises an error if text is not provided' do
-    icon_color = 'blue'
-    icon = 'nexmo-circle'
-    header = 'My header'
-
     expect do
       render partial: '/static/default_landing/partials/structured_text.html.erb', locals: {
-        'icon_color' => icon_color,
-        'icon' => icon,
-        'header' => header,
+        'icon_color' => 'blue',
+        'icon' => 'nexmo-circle',
+        'header' => 'My header',
+        'center_text' => false,
     }
     end .to raise_error("Missing 'text' key in structured_text landing page block")
   end
