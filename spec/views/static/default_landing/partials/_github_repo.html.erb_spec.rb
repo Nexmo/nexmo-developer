b788310f519d26b59515f477dafb6f98f3770873
benhayehudi <bengreenberg@gmail.com>
2019-02-07 09:43:57 +0200

    passing variable definitions directly into github_repo partial tests

--- spec/views/static/default_landing/partials/_github_repo.html.erb_spec.rb
+++ spec/views/static/default_landing/partials/_github_repo.html.erb_spec.rb
@@ -2,14 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'rendering _github_repo landing page partial' do
   it 'renders correctly' do
-    repo_url = 'https://example.com/org/repo-name'
-    github_repo_title = 'This is a sample title'
-    language = 'Ruby'
-
     render partial: '/static/default_landing/partials/github_repo.html.erb', locals: {
-        'repo_url' => repo_url,
-        'github_repo_title' => github_repo_title,
-        'language' => language,
+        'repo_url' => 'https://example.com/org/repo-name',
+        'github_repo_title' => 'This is a sample title',
+        'language' => 'Ruby',
     }
 
     expect(rendered).to include('<a class="Vlt-card Nxd-github-card Vlt-left" href="https://example.com/org/repo-name" data-github="Nexmo/repo-name">')
@@ -19,37 +15,28 @@ RSpec.describe 'rendering _github_repo landing page partial' do
   end
 
   it 'raises an error if repo_url key is missing' do
-    github_repo_title = 'This is a sample title'
-    language = 'Ruby'
-
     expect do
       render partial: '/static/default_landing/partials/github_repo.html.erb', locals: {
-      'github_repo_title' => github_repo_title,
-      'language' => language,
+        'github_repo_title' => 'This is a sample title',
+        'language' => 'Ruby',
       }
     end.to raise_error("Missing 'repo_url' key in github_repo landing page block")
   end
 
   it 'raises an error if github_repo_title key is missing' do
-    repo_url = 'https://example.com/org/repo-name'
-    language = 'Ruby'
-
     expect do
       render partial: '/static/default_landing/partials/github_repo.html.erb', locals: {
-      'repo_url' => repo_url,
-      'language' => language,
+        'repo_url' => 'https://example.com/org/repo-name',
+        'language' => 'Ruby',
       }
     end.to raise_error("Missing 'github_repo_title' key in github_repo landing page block")
   end
 
   it 'raises an error if language key is missing' do
-    repo_url = 'https://example.com/org/repo-name'
-    github_repo_title = 'This is a sample title'
-
     expect do
       render partial: '/static/default_landing/partials/github_repo.html.erb', locals: {
-      'repo_url' => repo_url,
-      'github_repo_title' => github_repo_title,
+        'repo_url' => 'https://example.com/org/repo-name',
+        'github_repo_title' => 'This is a sample title',
       }
     end.to raise_error("Missing 'language' key in github_repo landing page block")
   end
