8fb557d9889dac120728eb9eb3346414a06e2d46
benhayehudi <bengreenberg@gmail.com>
2019-02-07 09:39:44 +0200

    passing variable definitions directly into action_button partial tests

--- spec/views/static/default_landing/partials/_action_button.html.erb_spec.rb
+++ spec/views/static/default_landing/partials/_action_button.html.erb_spec.rb
@@ -2,26 +2,24 @@ require 'rails_helper'
 
 RSpec.describe 'rendering _action_button landing page partial' do
   it 'renders correctly with local variables' do
-    url_input = '#a-sample-url'
-    text_input = 'Click here!',
-                 large = false
-    type = 'primary'
-
-    render partial: '/static/default_landing/partials/action_button.html.erb', locals: { 'url' => url_input, 'text' => text_input, 'large' => large, 'type' => type }
+    render partial: '/static/default_landing/partials/action_button.html.erb', locals: {
+      'url' => '#a-sample-url',
+      'text' => 'Click here!',
+      'large' => false,
+      'type' => 'primary',
+    }
 
     expect(rendered).to include('<a class="Vlt-btn Vlt-btn--primary " href="#a-sample-url">')
     expect(rendered).to include('Click here!')
   end
 
   it 'ignores local variables provided to it in the rendering if not used' do
-    url_input = '#a-sample-url'
-    text_input = 'Click here!'
-    large = false
-    type = 'primary'
-    another_variable = 'Ignore me'
-
     render partial: '/static/default_landing/partials/action_button.html.erb', locals: {
-      'url' => url_input, 'text' => text_input, 'another_variable' => another_variable, 'large' => large, 'type' => type
+      'url' => '#a-sample-url',
+      'text' => 'Click here!',
+      'large' => false,
+      'type' => 'primary',
+      'another_variable' => 'Ignore me',
     }
 
     expect(rendered).to include('<a class="Vlt-btn Vlt-btn--primary " href="#a-sample-url">')
@@ -30,18 +28,22 @@ RSpec.describe 'rendering _action_button landing page partial' do
   end
 
   it 'raises an error if url is not provided' do
-    text_input = 'Click here!'
-    large = false
-    type = 'primary'
-
-    expect { render partial: '/static/default_landing/partials/action_button.html.erb', locals: { 'text_input' => text_input, 'large' => large, 'type' => type } }.to raise_error("missing 'url' key in action_button landing page block")
+    expect do
+      render partial: '/static/default_landing/partials/action_button.html.erb', locals: {
+      'text' => 'Click here!',
+      'large' => false,
+      'type' => 'primary',
+      }
+    end    .to raise_error("missing 'url' key in action_button landing page block")
   end
 
-  it 'raises an error if text_input is not provided' do
-    url = '#a-sample-url'
-    large = false
-    type = 'primary'
-
-    expect { render partial: '/static/default_landing/partials/action_button.html.erb', locals: { 'url' => url, 'large' => large, 'type' => type } }.to raise_error("missing 'text' key in action_button landing page block")
+  it 'raises an error if text is not provided' do
+    expect do
+      render partial: '/static/default_landing/partials/action_button.html.erb', locals: {
+      'url' => '#a-sample-url',
+      'large' => false,
+      'type' => 'primary',
+      }
+    end    .to raise_error("missing 'text' key in action_button landing page block")
   end
 end
