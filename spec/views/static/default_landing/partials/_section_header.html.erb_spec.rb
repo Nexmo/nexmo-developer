1768a232afd0581369455526dd9dd97eb9c5b6fb
benhayehudi <bengreenberg@gmail.com>
2019-02-07 09:51:16 +0200

    passing variable definitions directly into section_header partial tests

--- spec/views/static/default_landing/partials/_section_header.html.erb_spec.rb
+++ spec/views/static/default_landing/partials/_section_header.html.erb_spec.rb
@@ -2,14 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'rendering _section_header landing page partial' do
   it 'renders correctly with local variable' do
-    icon_color = 'blue'
-    icon = 'an-icon'
-    title = 'Here is a title'
-
     render partial: '/static/default_landing/partials/section_header.html.erb', locals: {
-        'icon_color' => icon_color,
-        'icon' => icon,
-        'title' => title,
+        'icon_color' => 'blue',
+        'icon' => 'an-icon',
+        'title' => 'Here is a title',
     }
 
     expect(rendered).to include('<svg class="Vlt-blue">')
@@ -18,37 +14,28 @@ RSpec.describe 'rendering _section_header landing page partial' do
   end
 
   it 'raises an error if icon_color is not provided' do
-    icon = 'an-icon'
-    title = 'Here is a title'
-
     expect do
       render partial: '/static/default_landing/partials/section_header.html.erb', locals: {
-        'icon' => icon,
-        'title' => title,
+        'icon' => 'an-icon',
+        'title' => 'Here is a title',
     }
     end .to raise_error("Missing 'icon_color' key in section_header landing page block")
   end
 
   it 'raises an error if icon is not provided' do
-    icon_color = 'blue'
-    title = 'Here is a title'
-
     expect do
       render partial: '/static/default_landing/partials/section_header.html.erb', locals: {
-        'icon_color' => icon_color,
-        'title' => title,
+        'icon_color' => 'blue',
+        'title' => 'Here is a title',
     }
     end .to raise_error("Missing 'icon' key in section_header landing page block")
   end
 
   it 'raises an error if a title is not provided' do
-    icon_color = 'blue'
-    icon = 'an-icon'
-
     expect do
       render partial: '/static/default_landing/partials/section_header.html.erb', locals: {
-        'icon_color' => icon_color,
-        'icon' => icon,
+        'icon_color' => 'blue',
+        'icon' => 'an-icon',
     }
     end .to raise_error("Missing 'title' key in section_header landing page block")
   end
