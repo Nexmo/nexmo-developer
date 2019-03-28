5c7ef88110d35b46e20723a3affc813dc7286f3b
benhayehudi <bengreenberg@gmail.com>
2019-03-28 13:07:03 +0000

    rubocop fixes

--- spec/controllers/admin_api/code_snippets_controller_spec.rb
+++ spec/controllers/admin_api/code_snippets_controller_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe AdminApi::CodeSnippetsController, :request do
-    before do
-      Rails.application.routes.draw do
-        get '/admin_api/code_snippets/' => 'code_snippets#index'
-      end
+  before do
+    Rails.application.routes.draw do
+      get '/admin_api/code_snippets/' => 'code_snippets#index'
     end
-    after do
-      Rails.application.reload_routes!
-    end
+  end
+  after do
+    Rails.application.reload_routes!
+  end
 
-    describe 'GET index' do
-        it 'does something' do
-            puts response
-        end
+  describe 'GET index' do
+    it 'does something' do
+      puts response
     end
+  end
 end
