c2334ddb0c574e45d62382ed40927b2b35af11cd
benhayehudi <bengreenberg@gmail.com>
2019-03-29 14:30:09 +0300

    added testing for date parameters calling the correct #created_between method

--- spec/controllers/admin_api/code_snippets_controller_spec.rb
+++ spec/controllers/admin_api/code_snippets_controller_spec.rb
@@ -5,33 +5,53 @@ RSpec.describe AdminApi::CodeSnippetsController, type: :request do
     allow_any_instance_of(AdminApiController).to receive(:authenticate).and_return(true)
   end
   describe 'GET #index' do
-    it 'retrieves all records when supplied with no params' do
-      @events = events_data
-      allow(UsageBuildingBlockEvent).to receive_message_chain(:all, :group, :count).and_return(@events)
+    context 'with no parameters' do
+      it 'retrieves all records when supplied with no params' do
+        @events = events_data
+        allow(UsageBuildingBlockEvent).to receive_message_chain(:all, :group, :count).and_return(@events)
 
-      get '/admin_api/code_snippets'
-      expect(@events.count).to eq(4)
+        get '/admin_api/code_snippets'
+        expect(@events.count).to eq(4)
+      end
     end
 
-    it 'retrieves only records matching the language parameter provided' do
-      @events = by_language_param
-      get '/admin_api/code_snippets?language=PHP'
-      allow(UsageBuildingBlockEvent).to receive_message_chain(:where, :group, :count).and_return(@events)
-      expect(@events.count).to eq(2)
+    context 'with block and/or language parameters' do
+      it 'retrieves only records matching the language parameter provided' do
+        @events = by_language_param
+        get '/admin_api/code_snippets?language=PHP'
+        allow(UsageBuildingBlockEvent).to receive_message_chain(:where, :group, :count).and_return(@events)
+        expect(@events.count).to eq(2)
+      end
+
+      it 'retrieves only records matching the block parameter provided' do
+        @events = by_block_param
+        get '/admin_api/code_snippets?block=voice/make-an-outbound-call'
+        allow(UsageBuildingBlockEvent).to receive_message_chain(:where, :group, :count).and_return(@events)
+        expect(@events.count).to eq(3)
+      end
+
+      it 'retrieves only records matching both block and language parameters provided' do
+        @events = by_language_and_block_params
+        get '/admin_api/code_snippets?block=voice/make-an-outbound-call&language=Ruby'
+        allow(UsageBuildingBlockEvent).to receive_message_chain(:where, :group, :count).and_return(@events)
+        expect(@events.count).to eq(1)
+      end
     end
 
-    it 'retrieves only records matching the block parameter provided' do
-      @events = by_block_param
-      get '/admin_api/code_snippets?block=voice/make-an-outbound-call'
-      allow(UsageBuildingBlockEvent).to receive_message_chain(:where, :group, :count).and_return(@events)
-      expect(@events.count).to eq(3)
-    end
+    context 'with date parameters' do
+      let(:params) do
+        {
+          'created_after' => '01/01/2019',
+          'created_before' => '03/01/2019',
+        }
+      end
+      it 'calls the #created_between method' do
+        @events = events_data
+        allow(UsageBuildingBlockEvent).to receive_message_chain(:created_between, :group, :count).and_return(@events)
 
-    it 'retrieves only records matching both block and language parameters provided' do
-      @events = by_language_and_block_params
-      get '/admin_api/code_snippets?block=voice/make-an-outbound-call&language=Ruby'
-      allow(UsageBuildingBlockEvent).to receive_message_chain(:where, :group, :count).and_return(@events)
-      expect(@events.count).to eq(1)
+        expect(UsageBuildingBlockEvent).to receive(:created_between).with(params['created_after'], params['created_before'])
+        get "/admin_api/code_snippets?created_after=#{params['created_after']}&created_before=#{params['created_before']}"
+      end
     end
   end
 end
