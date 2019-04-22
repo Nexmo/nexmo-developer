bb4102beac83634c4b10da33eb74c87e4947b1c3
benhayehudi <bengreenberg@gmail.com>
2019-04-22 08:49:04 +0300

    rubocop fix

--- lib/common_errors.rb
+++ lib/common_errors.rb
@@ -1,5 +1,5 @@
 class CommonErrors
   def self.check_for_migration_error(body)
-    raise 'DB Migration is pending. Please run RAILS_ENV=development bin/rake db:migrate' if body && body.downcase.include?('migrations are pending')
+    raise 'DB Migration is pending. Please run RAILS_ENV=development bin/rake db:migrate' if body&.downcase&.include?('migrations are pending')
   end
 end
