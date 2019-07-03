38b50715f9cdd6bdc3f1cc3066fa1db321d8674d
Michael Heap <m@michaelheap.com>
2019-07-03 09:50:07 +0100

    Remove Turbolinks
    Turbolinks has been causing issues where JS isn't executed as it's not
    a real page refresh. This means we keep adding workarounds for third
    party services
    
    This commit removes Turbolinks, and as far as I can tell it doesn't have
    a negative performance impact.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,7 +16,6 @@ import GithubCards from '../github_cards'
 import VoltaTabbedExamples from '../volta_tabbed_examples'
 import Format from '../format'
 import JsSequenceDiagrams from '../js_sequence_diagrams'
-import Navigation from '../navigation'
 import Scroll from '../scroll'
 import Search from '../components/search'
 import Notices from '../notices'
@@ -26,17 +25,8 @@ import APIStatus from '../api_status'
 import CodeSnippetEvents from '../code_snippet_events'
 import JWTGenerator from '../components/jwt_generator'
 
-import {
-  preventSamePage as turbolinksPreventSamePage,
-  animate as turbolinksAnimate
-} from '../turbolinks'
-
-Navigation()
-Scroll()
-turbolinksPreventSamePage()
-turbolinksAnimate()
-
-let refresh = () => {
+$(document).ready(function() {
+  Scroll()
   Notices()
   GithubCards()
   JsSequenceDiagrams()
@@ -44,7 +34,6 @@ let refresh = () => {
   new Format
   APIStatus()
   Scroll()
-  Navigation()
   CodeSnippetEvents()
 
   if (document.getElementById('jwtGenerator')) {
@@ -67,15 +56,7 @@ let refresh = () => {
   let rightPane = document.querySelector(".Vlt-main");
   if (rightPane) { rightPane.click(); }
 
-  Volta.init(['accordion', 'tooltip', 'tab', 'modal', 'dropdown'])
-
-  // Fix for Turbolinks scrolling to in-page anchor when navigating to a new page
-  if(window.location.hash){
-    const tag = document.getElementById(window.location.hash.slice(1))
-    if(tag){
-      tag.scrollIntoView(true);
-    }
-  }
+  Volta.init(['accordion', 'tooltip', 'tab', 'modal', 'dropdown', 'menu'])
 
   setTimeout(function() {
     const sidebarActive = document.querySelector('.Vlt-sidemenu__link_active')
@@ -102,9 +83,5 @@ let refresh = () => {
     }
     $(this).text(newText);
   });
-}
-
-$(document).on('nexmo:load', function() {
-  refresh();
-})
+});
 
